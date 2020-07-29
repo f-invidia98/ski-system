c5c708571bf13bab9cdf7ba267474fb7e163beab
Francesco <francesco.invidia.98@gmail.com>
2020-07-29 15:51:04 +0200

    d

--- ski_script.js
+++ ski_script.js
@@ -1,12 +1,3 @@
-window.onload = function(){
-    var xhttp = new XMLHttpRequest();
-
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-             document.getElementById('nav-placeholder').innerHTML= '<object type="text/html" data="nav.html"></object>';
-        }
-    }
-
-    xhttp.open('POST', 'nav.html', true); // method, location, async
-    xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhttp.send(); }
+$(function() {
+  $("#nav-placeholder").load("nav.html");
+});
