a088795214176b6a44b510e7d0702105843f33e3
Francesco <francesco.invidia.98@gmail.com>
2020-07-29 17:22:17 +0200

    d

--- ski_script.js
+++ ski_script.js
@@ -1,3 +1,19 @@
 $(function() {
   $("#nav-placeholder").load("nav.html");
 });
+
+$(".botton").click(function(){
+  if ($(".menu").css("display") == 'block') {
+    $(".menu").css("display", "none")
+  }else if($(".menu").css("display") == 'none') {
+    $(".menu").css("display", "block")
+  }
+})
+
+$(window).resize(function() {
+if ($(window).width() > 375) {
+
+    $(".menu").css("display", "none")
+
+}
+})
