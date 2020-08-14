2740c57f817495d50c9f1caff27462b58e805bd7
Francesco <francesco.invidia.98@gmail.com>
2020-08-14 11:16:39 +0200

    aggiornamento 1

--- ski_script.js
+++ ski_script.js
@@ -3,25 +3,25 @@
 // });
 
 
-var language;
-var lang = 'en';
-function getLanguage() {
-(localStorage.getItem('language') == null) ? setLanguage('en') : false;
-$.ajax({
-url:  'language/' +  localStorage.getItem('language') + '.json',
-dataType: 'json', async: false, dataType: 'json',
-success: function (lang) { language = lang } });
-}
-
-function setLanguage(lang) {
-localStorage.setItem('language', lang);
-}
-
-
-$(document).ready(function(){
-    getLanguage();
-    $('.cont_text').text(language.date);
-    });
+// var language;
+// var lang = 'en';
+// function getLanguage() {
+// (localStorage.getItem('language') == null) ? setLanguage('en') : false;
+// $.ajax({
+// url:  'language/' +  localStorage.getItem('language') + '.json',
+// dataType: 'json', async: false, dataType: 'json',
+// success: function (lang) { language = lang } });
+// }
+//
+// function setLanguage(lang) {
+// localStorage.setItem('language', lang);
+// }
+//
+//
+// $(document).ready(function(){
+//     getLanguage();
+//     $('.cont_text').text(language.date);
+//     });
 
 $(".botton").click(function(){
   if ($(".menu").css("display") == 'block') {
