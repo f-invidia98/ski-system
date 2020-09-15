d75523c93c3bba6cd4323899f33623c65ce0630e
Francesco <francesco.invidia.98@gmail.com>
2020-09-15 13:58:43 +0200

    prova altezza

--- vid_backgound.js
+++ vid_backgound.js
@@ -1,3 +1,13 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+
 var vid = document.getElementById('exampleVideo');
 
 	vid.play();
