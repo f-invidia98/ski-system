0884cbd2dcc0992b028495dfc039e299fc51e225
Francesco <francesco.invidia.98@gmail.com>
2021-03-01 20:50:31 +0100

    ultime correzioni

--- ski_script.js
+++ ski_script.js
@@ -89,10 +89,12 @@ $('#lang > a').click(function(){
   $('#lab_text').text(language.text_lab);
   $('#noleggio_text').text(language.noleggio_text);
   $('#tutti_text').text(language.tutti_text);
-
   $('.t_noleggio').text(language.t_noleggio);
   $('.t_contatti').text(language.t_contatti);
   $('.t_dolomiti').text(language.t_dolomiti);
+  $('.t_sci').text(language.t_sci);
+  $('.t_tar').text(language.t_tar);
+  $('.t_pre').text(language.t_pre);
   $('.text_paolo').text(language.text_paolo);
   $('#text_walter').text(language.text_walter);
   $('#text_dolomiti').text(language.text_dolomiti);
@@ -119,14 +121,17 @@ $('#lang > a').click(function(){
      $('#lab_text').text(language.text_lab);
      $('#noleggio_text').text(language.noleggio_text);
      $('#tutti_text').text(language.tutti_text);
-     $('.noleggio_titolo').text(language.noleggio_titolo);
      $('.t_noleggio').text(language.t_noleggio);
      $('.t_contatti').text(language.t_contatti);
      $('.t_dolomiti').text(language.t_dolomiti);
+     $('.t_sci').text(language.t_sci);
+     $('.t_tar').text(language.t_tar);
+     $('.t_pre').text(language.t_pre);
      $('.text_paolo').text(language.text_paolo);
      $('#text_walter').text(language.text_walter);
      $('#text_dolomiti').text(language.text_dolomiti);
      $('#text_dolomiti_cred').text(language.text_dolomiti_cred);
+
      });
 
 
