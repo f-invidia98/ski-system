35ce4b32da5f2f472b39e033c58f334fb7068e4e
Francesco <francesco.invidia.98@gmail.com>
2021-04-06 15:39:34 +0200

    non so

--- vid_backgound.js
+++ vid_backgound.js
@@ -8,94 +8,93 @@ window.addEventListener('resize', () => {
 });
 
 
+
 var vid = document.getElementById('exampleVideo');
 
 
-if (vid.readyState === 4) {
-  vid.addEventListener("canplay", function() {
-    setTimeout(function() {
+// function vidload() {
+//   if (vid.readyState == 4) {
+//     vid.play();
+//   }
+// };
+
+
+
+
+
+
+vid.addEventListener("canplay", function() {
+    if (vid.readyState == 4) {
       vid.play();
-    }, 1000);
-  });
+    }
+  // setTimeout(function() {
+  //   vid.play();
+  // }, 1000);
+});
+// vid.play();
+
+
+var wrapper = document.getElementById('wrapper');
+var canvas = document.getElementById('exampleCanvas');
+var ctx = canvas.getContext('2d');
+
+var ratio = window.devicePixelRatio || 1;
+var vidWidth;
+var vidHeight;
+
+vid.onloadedmetadata = function() {
+  vidWidth = vid.videoWidth;
+  vidHeight = vid.videoHeight;
+  canvas.width = vid.offsetWidth * ratio * 2;
+  canvas.height = vid.offsetHeight * ratio * 2;
+  // canvas.style.width = vid.offsetWidth*3 + "px";
+  canvas.style.height = vid.offsetHeight * 2 + "px";
+  canvas.style.width = "70vw";
+  drawingLoop();
+  setTimeout(() => {
+    setVideoBgColor(vid, wrapper);
+  }, 1300);
+};
+
+function drawingLoop() {
+  requestId = window.requestAnimationFrame(drawingLoop)
+  ctx.drawImage(vid, 0, 0, vidWidth, vidHeight, // source rectangle
+    0, 0, canvas.width, canvas.height); // destination rectangle);
+}
+
+function setVideoBgColor(vid, backgroundElement) {
+  // draw first four pixel of video to a canvas
+  // then get pixel color from that canvas
+  var canvas = document.createElement("canvas");
+  canvas.width = 8;
+  canvas.height = 8;
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(vid, 0, 0, 8, 8);
+  var p = ctx.getImageData(0, 0, 8, 8).data;
+  //dont take the first but fourth pixel [r g b]
+  backgroundElement.style.backgroundColor = "rgb(" + p[60] + "," + p[61] + "," + p[62] + ")";
 }
 
 
+window.onresize = function(event) {
+  vidWidth = vid.videoWidth;
+  vidHeight = vid.videoHeight;
 
-  // vid.play();
+  canvas.width = vid.offsetWidth;
+  canvas.height = vid.offsetHeight;
 
-
-	var wrapper = document.getElementById('wrapper');
-	var canvas = document.getElementById('exampleCanvas');
-	var ctx = canvas.getContext('2d');
-
-	var ratio = window.devicePixelRatio || 1;
-	var vidWidth;
-	var vidHeight;
-
-	vid.onloadedmetadata = function() {
-		vidWidth = vid.videoWidth;
-		vidHeight = vid.videoHeight;
-
-		canvas.width = vid.offsetWidth * ratio*2;
-	  canvas.height = vid.offsetHeight * ratio*2;
-
-	// canvas.style.width = vid.offsetWidth*3 + "px";
-	canvas.style.height = vid.offsetHeight*2 + "px";
-
-	canvas.style.width =  "70vw";
-	// canvas.style.height = "100vh";
+  //redraw canvas after resize
+  ctx.drawImage(vid, 0, 0, vidWidth, vidHeight, // source rectangle
+    0, 0, canvas.width, canvas.height); // destination rectangle);
+};
 
 
 
-		drawingLoop();
-
-		setTimeout(() => {
-			setVideoBgColor(vid, wrapper);
-
-		}, 1300);
-	};
-
-	function drawingLoop(){
-	  requestId = window.requestAnimationFrame(drawingLoop)
-
-	  ctx.drawImage(vid, 0, 0, vidWidth, vidHeight, // source rectangle
-	                   0, 0, canvas.width, canvas.height); // destination rectangle);
-	}
-
-	function setVideoBgColor(vid, backgroundElement) {
-
-	            // draw first four pixel of video to a canvas
-	            // then get pixel color from that canvas
-	            var canvas = document.createElement("canvas");
-	            canvas.width = 8;
-	            canvas.height = 8;
-
-	            var ctx = canvas.getContext("2d");
-	            ctx.drawImage(vid, 0, 0, 8, 8);
-
-	            var p = ctx.getImageData(0, 0, 8, 8).data;
-	            //dont take the first but fourth pixel [r g b]
-	            backgroundElement.style.backgroundColor = "rgb(" + p[60] + "," + p[61] + "," + p[62] + ")";
-
-	}
 
 
-	window.onresize = function(event) {
-	    vidWidth = vid.videoWidth;
-	    vidHeight = vid.videoHeight;
-
-	    canvas.width = vid.offsetWidth;
-	    canvas.height = vid.offsetHeight;
-
-	    //redraw canvas after resize
-	    ctx.drawImage(vid, 0, 0, vidWidth, vidHeight, // source rectangle
-	                   0, 0, canvas.width, canvas.height); // destination rectangle);
-	};
-
-
-	$(document).ready(function() {
-	  setTimeout(function(){
-	    window.location.href = "home.html";
-	  },6500)
-
-	  });
+// $(document).ready(function() {
+//   setTimeout(function(){
+//     window.location.href = "home.html";
+//   },6500)
+//
+//   });
